af59ece53cda9ef98567166e0ec5c30a86352429
iMac 8 <mazzone.gaston@gmail.com>
2018-05-17 19:58:13 -0300

    Initial Commit

---
+++ Trabajo Entregable 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trabajo Entregable 1
+//
+//  Created by SP20 on 17/5/18.
+//  Copyright © 2018 SP20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
